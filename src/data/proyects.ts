d289f2585b36e5266636471c4be6a312f3b58910
Denilson <jordanpreseleccion@gmail.com>
2024-05-10 00:58:22 -0500

    Added navbar sub options, only desktop view

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -1,23 +1,25 @@
+import SistemasEmbebidos from '@/assets/Inicio/DataAdq.webp'
+
 export const proyects = [
   {
     title: 'Sistema de Adquisición de Datos',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
-    image: 'https://via.placeholder.com/150',
+    image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición',
-      'Gran alcance de medición',
-      'Gran alcance de medición',
-      'Gran alcance de medición'
+      'Gran alcance de medición 1',
+      'Gran alcance de medición 2',
+      'Gran alcance de medición 3',
+      'Gran alcance de medición 4'
     ],
     nextPage: '#'
   },
   {
     title: 'Maquinaria Industrial',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
-    image: 'https://via.placeholder.com/150',
+    image: SistemasEmbebidos,
     highs: [
       'Gran alcance de medición',
-      'Gran alcance de medición',
+      'Gran alcance de medición 2',
       'Gran alcance de medición',
       'Gran alcance de medición'
     ],
@@ -26,11 +28,11 @@ export const proyects = [
   {
     title: 'Sistema de Medición de Energía',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
-    image: 'https://via.placeholder.com/150',
+    image: SistemasEmbebidos,
     highs: [
       'Gran alcance de medición',
       'Gran alcance de medición',
-      'Gran alcance de medición',
+      'Gran alcance de medición 3',
       'Gran alcance de medición'
     ],
     nextPage: '#'
@@ -38,9 +40,9 @@ export const proyects = [
   {
     title: 'Sistema de Control y Monitoreo de Energía',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
-    image: 'https://via.placeholder.com/150',
+    image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición',
+      'Gran alcance de medición 1',
       'Gran alcance de medición',
       'Gran alcance de medición',
       'Gran alcance de medición'
@@ -50,12 +52,12 @@ export const proyects = [
   {
     title: 'Controlador Industrial para Minería',
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
-    image: 'https://via.placeholder.com/150',
+    image: SistemasEmbebidos,
     highs: [
       'Gran alcance de medición',
       'Gran alcance de medición',
       'Gran alcance de medición',
-      'Gran alcance de medición'
+      'Gran alcance de medición 4'
     ],
     nextPage: '#'
   }
