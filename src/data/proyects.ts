bf9b6746c3a82b9e5c735046ef002e01cb462251
Denilson <jordanpreseleccion@gmail.com>
2024-05-09 19:17:26 -0500

    slider pryects done

---
+++ src/data/proyects.ts
@@ -0,0 +1,62 @@
+export const proyects = [
+  {
+    title: 'Sistema de Adquisición de Datos',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    image: 'https://via.placeholder.com/150',
+    highs: [
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición'
+    ],
+    nextPage: '#'
+  },
+  {
+    title: 'Maquinaria Industrial',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    image: 'https://via.placeholder.com/150',
+    highs: [
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición'
+    ],
+    nextPage: '#'
+  },
+  {
+    title: 'Sistema de Medición de Energía',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    image: 'https://via.placeholder.com/150',
+    highs: [
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición'
+    ],
+    nextPage: '#'
+  },
+  {
+    title: 'Sistema de Control y Monitoreo de Energía',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    image: 'https://via.placeholder.com/150',
+    highs: [
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición'
+    ],
+    nextPage: '#'
+  },
+  {
+    title: 'Controlador Industrial para Minería',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    image: 'https://via.placeholder.com/150',
+    highs: [
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición',
+      'Gran alcance de medición'
+    ],
+    nextPage: '#'
+  }
+]
