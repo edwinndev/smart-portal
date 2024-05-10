07b7b6387f58d4eabdcea217cf3c484e26eb4d86
Denilson <jordanpreseleccion@gmail.com>
2024-05-10 16:41:06 -0500

    building services software page

--- src/data/proyects.ts
+++ src/data/proyects.ts
@@ -3,61 +3,61 @@ import SistemasEmbebidos from '@/assets/Inicio/DataAdq.webp'
 export const proyects = [
   {
     title: 'Sistema de Adquisición de Datos',
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    description: `Nuestro sistema de adquisición de datos es una solución para monitorear el entorno ambiental. Alimentado por energía solar registra con precisión la temperatura, humedad y nivel, almacenando los datos en una memoria SD con marca de tiempo. Nuestro software de adquisición facilita la lectura, descarga y configuración del equipo, que opera de manera autónoma y utiliza el protocolo Modbus RTU para comunicarse a través de RS485.`,
     image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición 1',
-      'Gran alcance de medición 2',
-      'Gran alcance de medición 3',
-      'Gran alcance de medición 4'
+      'Equipo autónomo',
+      'Registrador de datos',
+      'Descarga de información a larga distancia',
+      'Reportes y analítica'
     ],
-    nextPage: '#'
+    nextPage: '/proyects/sistema-adquisicion-datos'
   },
   {
     title: 'Maquinaria Industrial',
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    description: `La máquina generadora de vapor para la industria del calzado es un equipo especializado diseñado para producir vapor de alta presión y temperatura. Este vapor se utiliza en varios procesos de fabricación de calzado, como el moldeado, el conformado y el tratamiento térmico de materiales. Esta máquina es fundamental para garantizar la calidad y durabilidad de los productos finales, permitiendo la manipulación precisa y eficiente de diferentes tipos de materiales utilizados en la industria del calzado.`,
     image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición',
-      'Gran alcance de medición 2',
-      'Gran alcance de medición',
-      'Gran alcance de medición'
+      'Óptimización del uso de agua',
+      'Ahorro de energía',
+      'Aumento en la productividad del calzado',
+      'Control automatizado mediante HMI'
     ],
-    nextPage: '#'
+    nextPage: '/proyects/maquinaria-industrial'
   },
   {
     title: 'Sistema de Medición de Energía',
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    description: `Equipo alimentado por batería, puede medir corriente AC de hasta 100 amperios, voltaje AC, potencia activa, reactiva, aparente y frecuencia. Los datos se almacenan en una tarjeta SD con marca de tiempo para un seguimiento preciso. La comunicación USB y el protocolo Modbus a través de RS485 facilitan la transferencia de datos a un software de adquisición, donde se grafican para un análisis detallado. Además, la conectividad WiFi permite la transferencia de datos a un servidor web para un acceso remoto conveniente.`,
     image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición',
-      'Gran alcance de medición',
-      'Gran alcance de medición 3',
-      'Gran alcance de medición'
+      'Análisis de calidad de energía',
+      'Conectividad WiFi, USB, RS485',
+      'Software de adquisición de datos',
+      'Equipo industrial'
     ],
-    nextPage: '#'
+    nextPage: '/proyects/sistema-medicion-energia'
   },
   {
-    title: 'Sistema de Control y Monitoreo de Energía',
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    title: 'Ecosistema para Gestión de Emergencias',
+    description: `El ecosistema de alertas integra un concentrador remoto con conectividad 4G LTE y GPS, enlazado con cinco botoneras inteligentes. Cada botonera tiene tres botones: Alerta, Precaución y Condiciones Normales. La comunicación entre el concentrador y las botoneras abarca hasta 200 metros por radio. Cuando se presiona un botón, la botonera notifica al concentrador, que transmite automáticamente la ubicación y estado a una plataforma web de gestión, facilitando la respuesta a emergencias.`,
     image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición 1',
-      'Gran alcance de medición',
-      'Gran alcance de medición',
-      'Gran alcance de medición'
+      'Botoneras IoT de bajo consumo de energía',
+      'Software de gestión de emergencias',
+      'Concentrador fácil de instalar en activos',
+      'Ecosistema escalable'
     ],
-    nextPage: '#'
+    nextPage: '/proyects/ecosistema-gestion-emergencias'
   },
   {
-    title: 'Controlador Industrial para Minería',
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ad rem, harum veritatis vel consequatur quia aut odit sed error laborum, corporis iusto excepturi incidunt sint quas illum eveniet deleniti.`,
+    title: 'Tablero de Control IoT para sistema BMS',
+    description: `Nuestro Tablero de control para Gestión de Edificios (BMS), con tres PLCs personalizados que ofrecen amplias capacidades de E/S y conectividad Ethernet, RS485, WIFI, RF y 4G LTE. Proporciona una interfaz centralizada para monitorear y controlar energía, climatización, iluminación y seguridad, con visualización en tiempo real, programación de horarios y ajustes remotos. Mejora la eficiencia operativa y la seguridad del edificio, permitiendo la integración con dispositivos inteligentes y plataformas de gestión..`,
     image: SistemasEmbebidos,
     highs: [
-      'Gran alcance de medición',
-      'Gran alcance de medición',
-      'Gran alcance de medición',
-      'Gran alcance de medición 4'
+      'Control y monitoreo remoto',
+      'Sensores descentrelizados',
+      'Diversas conectividades',
+      'Equipos estandarizados'
     ],
     nextPage: '#'
   }
