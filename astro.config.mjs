551106179ae8dd565754ca307525d33f57ea7cb1
Edwin <edwinn.dev@gmail.com>
2024-05-27 18:51:57 -0500

    feat: Add Dockerfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'static',
+  site: 'https://smartelectronics.com',
+  server: {
+    port: 3000
+  }
 });
